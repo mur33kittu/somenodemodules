d8ca976ed949b5035f9cd620f1b7fdac7bd4cc86
Murthy G <31370582+mur33kittu@users.noreply.github.com>
2018-06-01 19:02:22 -0400

    Add files via upload

---
+++ ngx-logger/esm2015/ngx-logger.js
@@ -0,0 +1,9 @@
+/**
+ * @fileoverview added by tsickle
+ * @suppress {checkTypes} checked by tsc
+ */
+/**
+ * Generated bundle index. Do not edit.
+ */
+export { LoggerModule, Levels, NGXLogger, LoggerConfig, CustomNGXLoggerService, NGXLoggerHttpService, NGXLoggerUtils, NgxLoggerLevel, HttpMetaDataInterface, NGXLoggerMock, CustomNGXLoggerServiceMock, NGXLoggerHttpServiceMock } from './public_api';
+//# sourceMappingURL=ngx-logger.js.map
